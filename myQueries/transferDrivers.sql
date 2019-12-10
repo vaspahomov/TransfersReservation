129072431ee408de436da3661f4f83ac0f6611ab
vaspahomov <vas2142553@gmail.com>
2019-12-11 01:16:24 +0500

    Fix cursor

--- myQueries/transferDrivers.sql
+++ myQueries/transferDrivers.sql
@@ -7,10 +7,10 @@ CREATE PROCEDURE CREATE_DRIVERS_LIST(
 BEGIN
     DECLARE FINISHED INT DEFAULT 0;
     DECLARE Driver_id MEDIUMINT DEFAULT 0;
-    DECLARE FirstName varchar(255) DEFAULT '';
-    DECLARE LastName varchar(255) DEFAULT '';
+    DECLARE C_FirstName varchar(255) DEFAULT '';
+    DECLARE C_LastName varchar(255) DEFAULT '';
 
-    DEClARE GET_DRIVER
+    DECLARE GET_DRIVER
         CURSOR FOR
         SELECT P_Id, FirstName, LastName FROM Persons WHERE P_Id IN (SELECT PersonId FROM Drivers);
 
@@ -21,11 +21,11 @@ BEGIN
 
     GET_DRIVERS:
     LOOP
-        FETCH GET_DRIVER INTO Driver_id, FirstName, LastName;
+        FETCH GET_DRIVER INTO Driver_id, C_FirstName, C_LastName;
         IF FINISHED = 1 THEN
             LEAVE GET_DRIVERS;
         END IF;
-        SET DRIVERS_LIST = CONCAT(Driver_id, ':', FirstName, ':', LastName, ';', DRIVERS_LIST);
+        SET DRIVERS_LIST = CONCAT(Driver_id, ':', C_FirstName, ':', C_LastName, ';', DRIVERS_LIST);
     END LOOP GET_DRIVERS;
     CLOSE GET_DRIVER;
 
